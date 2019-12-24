8539169aee290aaa29ed13632253b0f64c32cd15
Unknown <b1802104@planet.kanazawa-it.ac.jp>
2019-12-24 18:25:58 +0900

    add:敵の妨害イベントを追加

---
+++ Assets/Script/enemibehavior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class enemibehavior : MonoBehaviour
+{
+
+    public static float CountDownTime;  // カウントダウンタイム
+
+    Button card;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CountDownTime = 100.0F;  // カウントダウン開始秒数をセット
+        card = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownTime -= Time.deltaTime;
+
+        if (CountDownTime <= 50.0F)
+        {
+            card.interactable = false;
+        }
+
+    }
+}
