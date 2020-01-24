b1698f783909f33d840d343c5e41d5ab5449ad8a
Spica <b1814707@planet.kanazawa-it.ac.jp>
2020-01-24 19:13:49 +0900

    add:カード削除追加

---
+++ Assets/Script/Disappear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Disappear : MonoBehaviour
+{
+
+    public static float CountDownTime;  // カウントダウンタイム
+
+    Button card;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CountDownTime = 100.0F;  // カウントダウン開始秒数をセット
+        card = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownTime -= Time.deltaTime;
+
+        if (CountDownTime <= 90.0F)
+        {
+            // 非表示にする
+            gameObject.SetActive(false);
+            Debug.Log("Button2 click!");
+        }
+
+    }
+}
